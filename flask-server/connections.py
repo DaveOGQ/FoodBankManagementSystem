82799d03024e555f57c83fa2e3cdd3d044a1e041
DaveOGQ <otigeorgedavid@gmail.com>
2024-01-03 03:53:43 -0700

    small edit

--- flask-server/connections.py
+++ flask-server/connections.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.config['MySQL_HOST'] = 'localhost' #default localhost port:5000
 app.config["MYSQL_USER"] = "root"   #username for MySQL database
-app.config["MYSQL_PASSWORD"] = "YourLocalHostMySqlPassword" #David's password
+app.config["MYSQL_PASSWORD"] = "YourLocalHostMySqlPassword" #password
 app.config["MYSQL_DB"] = "foodbankdb"    #name of the database
 
 mysql = MySQL(app)      #connect MySql to the app
